a94df982016768159cb3fa46fdecc0d76888e40d
sandeepkumaar <cskumaar1992@gmail.com>
2023-08-06 12:31:52 +0530

    add css, templates, mocks

--- mocks/services.js
+++ mocks/services.js
@@ -1,98 +1,49 @@
 import { rest } from 'msw';
 import { delayRes } from './utils';
 
-import localforage from "localforage";
-import { matchSorter } from "match-sorter";
-import sortBy from "sort-by";
 
 const host = 'http://localhost:4000'
 
-function set(contacts) {
-  return localforage.setItem("contacts", contacts);
+import { questions } from './quiz-list.js'
+
+const randomiseArray = function(arr=[], count=0) {
+  let tempArray = arr.slice(0);
+  let randomArray = [];
+  //let loopCount = 0;
+  while(count-- > 0) {
+    //loopCount++;
+    // when we reach the last element in tempArray
+    if(tempArray.length === 1) {
+      randomArray.push(tempArray.pop());
+      break;
+    }
+    // if randon index is last index, retry
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * tempArray.length)
+    } while (randomIndex === tempArray.length - 1 )
+
+    let randomItem = tempArray[randomIndex];
+    tempArray[randomIndex] = tempArray.pop();
+    randomArray.push(randomItem);
+    //console.log({ loopCount, randomIndex, randomItem, tempArray, randomArray, tempLength: tempArray.length });
+  };
+  //console.log('final',  { tempArray, randomArray});
+  return randomArray;
 };
 
-
-const getContacts = rest.get(`${host}/api/contacts`, async function(req, res, ctx) {
-  const { q: query } = Object.fromEntries(req.url.searchParams);
-  let contacts = await localforage.getItem('contacts');
-  if(!contacts) contacts = [];
-  if(query) {
-    contacts = matchSorter(contacts, query, {keys: ["first", "last"]});
-  };
-  let contactList = contacts.sort(sortBy("last", "createdAt"));
+const fetchRandomQuestions = rest.get(`${host}/random/questions`, async function(req, res, ctx) {
+  const { q: count } = Object.fromEntries(req.url.searchParams);
+  let randomQuiz = randomiseArray(questions, Number(count));
   return delayRes(
     ctx.status(200),
-    ctx.json(contactList)
-  );
-});
-
-const createContact = rest.post(`${host}/api/contacts/create`, async function(req, res, ctx) {
-  let contact = req.body;
-  let contacts = await localforage.getItem('contacts');
-  if(!contacts) contacts = [];
-  contacts.unshift(contact);
-  await set(contacts);
-  return delayRes(
-    ctx.status(200),
-    ctx.json(contact)
-  );
-  
+    ctx.json(randomQuiz)
+  )
 })
 
-const getContact = rest.get(`${host}/api/contacts/:id`, async function(req, res, ctx) {
-  let { id } = req.params;
-  let contacts = await localforage.getItem('contacts');
-  let contact = contacts.find(contact => contact.id === id);
-  return delayRes(
-    ctx.status(200),
-    ctx.json(contact)
-  )
-
-});
-
-const updateContact = rest.put(`${host}/api/contacts/:id`, async function(req, res, ctx) {
-  let { id } = req.params;
-  let updates = req.body;
-  let contacts = await localforage.getItem("contacts");
-  let contact = contacts.find(contact => contact.id === id);
-  if (!contact) throw new Error("No contact found for", id);
-  Object.assign(contact, updates);
-  await set(contacts);
-  return delayRes(
-    ctx.status(200),
-    ctx.json(contact)
-  );
-});
-
-const deleteContact = rest.delete(`${host}/api/contacts/:id`, async function(req, res, ctx) {
-  let { id } = req.params;
-  let contacts = await localforage.getItem("contacts");
-  let index = contacts.findIndex(contact => contact.id === id);
-  let deleteStatus = false;
-  if (index > -1) {
-    contacts.splice(index, 1);
-    await set(contacts);
-    deleteStatus = true;
-  };
-  return delayRes(
-    ctx.status(200),
-    ctx.json({status: deleteStatus})
-  )
-});
-
-const deleteContactError = rest.delete(`${host}/api/contacts/:id`, async function(req, res, ctx) {
-  return delayRes(
-    ctx.status(500),
-    ctx.json({status: false})
-  );
-});
-
 const handlers = [
-  getContacts,
-  createContact,
-  getContact,
-  updateContact,
-  deleteContact,
+  fetchRandomQuestions,
+
 ];
 
 export default handlers;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -9,6 +9,32 @@ export function Action({request}) {
   //
 }
 
+function LandingPage() {
+  return (
+    <>
+      <h4 className='h4 text-center mb-4 mt-4'>Welcome to Roman Empire</h4>
+      <section className='instruction card card--border flex-center-lr mb-2'>
+        <h5 className='h5 mb-1'> Instructions</h5>
+        <ul className='sub-2 ml-2'>
+          <li>=> Each question has 4 multiple choices to choose from</li>
+          <li>=> You have 30 seconds to answer</li>
+          <li>=> You will be provided with a hint for the last 10 seconds.</li>
+        </ul>
+      </section>
+      <section className='start-form'>
+        <form className='form'>
+          <p className='mb-1'> Please Enter the number of questions</p>
+          <div className='flex justify-content-evenly'>
+            <input className='input-sm input--border' type='number' required value='0'></input>
+            <button className='btn-md btn--border primary'>START</button>
+          </div>
+        </form>
+      </section>
+    </>
+
+  )
+}
+
 export default function App() {
   const loaderData = useLoaderData();
   return (
@@ -18,23 +44,22 @@ export default function App() {
       </header>
 
       <main className="app__main main background align-items-center">
-        <h4 className='h4 text-center mb-4 mt-4'>Welcome to Roman Empire</h4>
-        <section className='instruction card card--border flex-center-lr mb-2'>
-          <h5 className='h5 mb-1'> Instructions</h5>
-          <ul className='sub-2 ml-2'>
-            <li>=> Each question has 4 multiple choices to choose from</li>
-            <li>=> You have 30 seconds to answer</li>
-            <li>=> You will be provided with a hint for the last 10 seconds.</li>
-          </ul>
+        <LandingPage />
+        <section className='feedback mb-4'>
         </section>
-        <section className='start-form'>
-          <form className='form'>
-            <p className='mb-1'> Please Enter the number of questions</p>
-            <div className='flex justify-content-evenly'>
-              <input className='input-sm input--border' type='number' required value='0'></input>
-              <button className='btn-md btn--border primary'>START</button>
+        <section className='question-answer card card--border'>
+          <p className='bold mb-3'> 
+            What famous Roman general and statesman was assassinated on the Ides of March in
+          44 BC?
+          </p>
+          <div className='input-group'>
+            <div className='input-container'>
+              <input type='radio' name='x' value='Options1' className='mr-1'></input>
+              <label for='x' className='color-black'>Options1</label>
             </div>
-          </form>
+          </div>
+          
+
         </section>
 
       </main>
